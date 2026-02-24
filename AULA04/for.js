6e3bec2b870271e57700673c4e0b29c2741d5deb
pietro <pietropardim0@gmail.com>
2026-02-24 10:30:33 -0300

    java script !

---
+++ AULA04/for.js
@@ -0,0 +1,27 @@
+//========================================
+//FOR
+//========================================
+
+//O for é usado quando queremos repetir um trecho de código várias vezes, geralmente sabendo *** QUANTAS VEZES *** ele deve se repetir 
+
+// for (início, condição, incremento) {
+//     //code que será repetido
+//     ddd;
+// }
+
+// //inicio ----> onde onde começa
+// //condição ----> até quando se repete
+// //incremento ----> como o valor muda a cada volta
+
+// for (let i=1; 1 <= 5; i++){
+//     console.log(i)
+
+// }
+
+// Crie uma função que imprima números de 1 até 10.
+
+function contar(){
+    for (let i=1; i<=10; i++)
+        console.log(i)
+}
+contar();
