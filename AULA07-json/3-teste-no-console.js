9ad3081c94032fb54d1a9cc4f78ab6368ffdae0a
pietro <pietropardim0@gmail.com>
2026-03-09 10:59:20 -0300

    newwx

---
+++ AULA07-json/3-teste-no-console.js
@@ -0,0 +1,65 @@
+//===================================
+// EXEMPLO DE CONSUMO DE API COM FETCH (PROMISE)
+// API usada: https://dog.ceo/api/breeds/image/radom
+// Essa API retorna uma imagem aleatória de cachorro 
+//===================================
+
+//TESTE 1: Fetch básico usando .then()
+
+//fetch() faz uma requisição HTTP para uma API externa
+
+fetch('http://dog.ceo/api/breeds/image/random')
+    //fetch retorna uma PROMISE (Algo que vai chegar depois)
+    .then(reposta => reposta.json())
+    // resposta.json() converte a resposta da API para JSON
+    // JSON é o formato padrão de dados usados em APIs
+
+    .then(dados =>{
+        //dados agora contem o objeto retornavel pela API
+        console.log('Dados completo do API:', dados);
+        //Mostrar a URL da imagem
+        console.log('URL da imagem', dados.message);   
+    })
+
+// ===============================
+// TESTE 2: Usando async / await
+// È uma forma mais moderna e mais fácil de ler
+// ===============================
+
+// Vamos criar uma funcção assíncrona
+// Função async permitem usar "await"
+async function testarAPI(){
+
+    //await significa: espere a resposta chegar antes de continuar
+    const resposta = await fetch('https://dog.ceo/api/breeds/image/random');
+
+    //Precisamos converter a resposta em JSON
+    const dados = await resposta.json();
+
+    console.log('Resposta da API:', dados);
+
+    console.log('Imagem do cachorro:', dados.message);
+    
+    
+}
+
+//chamamos a funcao para executar o teste
+testarAPI();
+
+//=====================================
+//exe classroom:
+//=====================================
+
+async function buscarHusky() {
+  try {
+    const response = await fetch("https://dog.ceo/api/breed/husky/images/random");
+    const data = await response.json();
+
+    console.log("🐺 Husky:", data.message);
+
+  } catch (erro) {
+    console.error("Erro ao buscar imagem:", erro);
+  }
+}
+
+buscarHusky();
